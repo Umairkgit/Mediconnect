d3dd411170a1369719becef1d0470b30243ee48e
Umair Khan <umairk9530@gmail.com>
2025-08-18 16:29:39 +0530

    second commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,26 +16,27 @@ const Navbar = () => {
   }
 
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+    <div className='flex items-center justify-between text-sm py-3 mb-4 border-b border-b-gray-400'>
       <img onClick={() => navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
-          <li className='py-1 '>HOME</li>
+          <li className='py-1 font-semibold hover:translate-y-[-3px] transition-all duration-500'>HOME</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' hidden />
         </NavLink>
+      
         <NavLink to='/doctors'>
-          <li className='py-1 '>ALL DOCTORS</li>
+          <li className='py-1 font-semibold hover:translate-y-[-3px] transition-all duration-500'>ALL DOCTORS</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' hidden />
         </NavLink>
         <NavLink to='/about'>
-          <li className='py-1 '>ABOUT</li>
+          <li className='py-1 font-semibold hover:translate-y-[-3px] transition-all duration-500'>ABOUT</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' hidden />
         </NavLink>
         <NavLink to='/contact'>
-          <li className='py-1 '>CONTACT</li>
+          <li className='py-1 font-semibold hover:translate-y-[-3px] transition-all duration-500'>CONTACT</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' hidden />
         </NavLink>
-    
+
       </ul>
 
       <div className='flex items-center gap-4'>
@@ -52,7 +53,7 @@ const Navbar = () => {
                 </div>
               </div>
             </div>
-            : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+            : <button onClick={() => navigate('/login')} className='bg-primary text-white px-6 py-2 rounded-full font-light hidden md:block hover:scale-105 transition-all duration-300'>Create Account</button>
         }
 
         <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
